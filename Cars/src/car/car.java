9402f149808ed8204d46f55fa9b88ae12e35a9ba
James Morin <jamesmorin@Macs-MacBook-3.local>
2016-03-21 12:39:42 -0400

    initial commit of cars

---
+++ Cars/src/car/car.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package car;
+
+/**
+ *
+ * @author jamesmorin
+ */
+public abstract class car {
+    
+    private String fuelType;
+    private int fuelLevel;
+    private int fuelPrice;
+
+    public car(String fuelType, int fuelLevel, int fuelPrice) {
+        this.fuelType = fuelType;
+        this.fuelLevel = fuelLevel;
+        this.fuelPrice = fuelPrice;
+    }
+    
+    public void honking(){
+        System.out.println("beep");
+    }
+    
+    public void driving(){
+        System.out.println("zoom");
+    }
+    
+    
+
+    public String getFuelType() {
+        return fuelType;
+    }
+
+    public void setFuelType(String fuelType) {
+        this.fuelType = fuelType;
+    }
+
+    public int getFuelLevel() {
+        return fuelLevel;
+    }
+
+    public void setFuelLevel(int fuelLevel) {
+        this.fuelLevel = fuelLevel;
+    }
+
+    public int getFuelPrice() {
+        return fuelPrice;
+    }
+
+    public void setFuelPrice(int fuelPrice) {
+        this.fuelPrice = fuelPrice;
+    }
+    
+    
+    
+ 
+    
+}
