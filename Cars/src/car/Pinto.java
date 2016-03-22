11ffa5a9bd5e3f2f8699b276edd88c5306f73cd8
James Morin <jamesmorin@Macs-MacBook-3.local>
2016-03-22 13:04:03 -0400

    commit of pinto class

--- Cars/src/car/Pinto.java
+++ Cars/src/car/Pinto.java
@@ -5,14 +5,55 @@
  */
 package car;
 
+import java.util.Scanner;
+
 /**
  *
  * @author jamesmorin
  */
 public class Pinto extends car{
     
+    Scanner sc = new Scanner(System.in);
+    
     public Pinto(String fuelType, int fuelLevel, int fuelPrice){
         super(fuelType, fuelLevel, fuelPrice);
+    }
+    
+    public Pinto(){
+        super("Gas", 14, 2);
+    }
+    
+    @Override
+    public void fuelUp(){
+        
+        System.out.println("What type of fuel would you like to add to your new Pinto?");
+        System.out.println("1: Gas \n"
+                + "2: Diesel \n"
+                + "3: Ethanol \n");
+        int userChoice = sc.nextInt();
+        
+        if (userChoice != 1){
+            System.out.println("Oh my Heavens! You KILLED it! (wrong type of gas)");
+        }else{
+            System.out.println("Thanks for fueling up, and enjoy driving your NEW CAR!");
+        }
+    
+    }
+    
+    @Override
+    public void goOnSale(){
+        System.out.println("Your Pinto is on sale today! It only costs " + (20000 * 0.75) + " What a Deal!");
+    }
+    
+    @Override
+    public void honk(){
+        System.out.println("beep!");
+    }
+    
+    @Override
+    public void drive(){
+        System.out.println("zoom!");
+        
     }
     
 }
